f0b71bb5d1b2c5ddadfe366b045e4333ebd60186
Alex <alexander.galdin92@gmail.com>
2018-01-30 19:25:27 +0200

    Setting up webpack

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import 'uikit/dist/css/uikit.min.css'
+import 'uikit/dist/js/uikit.min.js'
+import 'uikit/dist/js/uikit-icons.min.js'
+import Vue from 'vue/dist/vue.min.js'
+
+require.context("./static/", true, /^\.\/.*\.json/);
+
+console.log('Index.js');
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './app/index.js',
+        configure: './app/configure.js'
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'CoughSyrup Useful Bookmarks',
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Configure your Useful Bookmarks',
+            filename: 'configure.html'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.json$/,
+                use: [
+                    'file-loader?name=[path][name].[ext]&context=./app/static'
+                ]
+            }
+        ]
+    }
+}
