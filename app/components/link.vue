5bd940e976c505a32359a4fb09ee8b74d3c55abc
Alex <alexander.galdin92@gmail.com>
2018-01-31 16:43:18 +0200

    Link template added with sample data

---
+++ app/components/link.vue
@@ -0,0 +1,19 @@
+<template>
+    <li>
+        <a class="uk-link" :href="link.reference">
+            <img class="uk-margin-small-right" :src="'chrome://favicon/' + link.reference"/>
+            {{ link.label }}
+        </a>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        link: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
--- app/components/links.vue
+++ app/components/links.vue
@@ -1,5 +1,35 @@
 <template>
     <ul class="uk-list uk-list-striped uk-nav">
-        <li>Link</li>
+        <li 
+            is="link-item"
+            v-for="link in links"
+            :key="link.id"
+            :link="link"></li>
     </ul>
 </template>
+
+<script>
+import linkItem from "./link.vue";
+
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    id: 0,
+                    label: "Test Link",
+                    reference: '#'
+                },
+                {
+                    id: 1,
+                    label: "Another Link",
+                    reference: '#'
+                }
+            ]
+        }
+    },
+    components: {
+        linkItem
+    }
+}
+</script>
